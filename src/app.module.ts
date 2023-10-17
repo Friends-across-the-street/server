0aecca7e7610bad37acd2d292baa09fdd1aea160
KKardd <wlstjd4013@gmail.com>
2023-10-17 17:24:22 +0900

    feat: set up for default configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [], // not yet
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
