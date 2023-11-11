8f76a7e5d00f24f9252f81970683925b94ba6ae2
KKardd <wlstjd4013@gmail.com>
2023-11-11 14:56:49 +0900

    feat: apply swagger for user services

--- src/incumbent_users/incumbent_users.controller.ts
+++ src/incumbent_users/incumbent_users.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { IncumbentUsersService } from './incumbent_users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('incumbent-users')
 @Controller('incumbent-users')
 export class IncumbentUsersController {
   constructor(private incumbentUsersService: IncumbentUsersService) {}
+
+  @ApiOperation({ summary: 'UserId로 자세한 현직자 정보 찾기' })
+  @ApiResponse({ status: 200, description: 'Success' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
   @Get('/:id')
   async findById(@Param('id') userId: number) {
     return await this.incumbentUsersService.findById(userId);
--- src/student_users/student_users.controller.ts
+++ src/student_users/student_users.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { StudentUsersService } from './student_users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('student-users')
 @Controller('student-users')
 export class StudentUsersController {
   constructor(private studentUsersService: StudentUsersService) {}
+  @ApiOperation({ summary: 'UserId로 자세한 학생 정보 찾기' })
+  @ApiResponse({ status: 200, description: 'Success' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
   @Get('/:id')
   async findById(@Param('id') userId: number) {
     return await this.studentUsersService.findById(userId);
