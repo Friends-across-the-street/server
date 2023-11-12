28b93b1ae57670d71dcba4708452d291e1842a1d
KKardd <wlstjd4013@gmail.com>
2023-11-12 21:05:47 +0900

    feat: add auth service(validate user, create token, verify token)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import authConfig from 'src/config/authConfig';
+import * as jwt from 'jsonwebtoken';
+import { PrismaService } from 'src/prisma.service';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(authConfig.KEY) private config: ConfigType<typeof authConfig>,
+    private prismaService: PrismaService,
+  ) {}
+
+  async validateUser(email, password) {
+    let user: any = await this.prismaService.incumbent_users.findFirst({
+      where: { email, password },
+    });
+    if (!user) {
+      user = await this.prismaService.student_users.findFirst({
+        where: { email, password },
+      });
+    }
+    if (!user) {
+      throw new NotFoundException('유저가 존재하지 않음');
+    }
+
+    return user;
+  }
+
+  async createToken(user: User) {
+    const payload = { ...user };
+    return jwt.sign(payload, this.config.jwtSecret, {
+      expiresIn: '1d',
+      audience: 'example.com',
+      issuer: 'example.com',
+    });
+  }
+
+  async verify(jwtToken: string) {
+    try {
+      const payload = jwt.verify(jwtToken, this.config.jwtSecret) as (
+        | jwt.JwtPayload
+        | string
+      ) &
+        User;
+
+      const { id, email } = payload;
+
+      return {
+        userId: id,
+        email,
+      };
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+}
