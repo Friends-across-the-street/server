f4f069d21ffbc67f96b2414aca3cbf352303105a
KKardd <wlstjd4013@gmail.com>
2023-11-16 23:45:52 +0900

    feat: add validation to login dto

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class UserLoginDto {
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   email: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   password: string;
 }
