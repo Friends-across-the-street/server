932ade690a18fc817b8292f3d2db76738109c939
KKardd <wlstjd4013@gmail.com>
2023-11-23 23:57:34 +0900

    feat: add interface about create-incumbent-user

---
+++ src/auth/interface/create-user.interface.ts
@@ -0,0 +1,14 @@
+export interface CreateIncumbentUserArgs {
+  email: string;
+  password: string;
+  name: string;
+  age: number;
+  gender: string;
+  image: string;
+  major: string;
+  school: string;
+  job_description: string;
+  company_name: string;
+  company_welfare: string;
+  company_location: string;
+}
