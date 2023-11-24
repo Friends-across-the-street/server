85752a3eb095b06504154bca817c77ec222f34f1
KKardd <wlstjd4013@gmail.com>
2023-11-24 11:40:23 +0900

    feat: configurate bcrypt salt round

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -2,4 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('auth', () => ({
   jwtSecret: process.env.JWT_SECRET,
+  bcryptSaltRound: process.env.BCRYPT_SALT_ROUNDS,
 }));
