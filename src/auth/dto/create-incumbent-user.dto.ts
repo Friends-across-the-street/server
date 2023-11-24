13ba137b81446571782169d7cfe655f6d2682776
KKardd <wlstjd4013@gmail.com>
2023-11-24 11:39:43 +0900

    feat: add validation in create incumbent user dto

--- src/auth/dto/create-incumbent-user.dto.ts
+++ src/auth/dto/create-incumbent-user.dto.ts
@@ -9,11 +9,13 @@ import {
 } from 'class-validator';
 
 export class CreateIncumbentUserDto {
+  @Transform((params) => params.value.trim())
   @IsString()
   @IsEmail()
   @MaxLength(60)
   readonly email: string;
 
+  @Transform((params) => params.value.trim())
   @IsString()
   @MinLength(2)
   @MaxLength(30)
@@ -26,16 +28,37 @@ export class CreateIncumbentUserDto {
   readonly name: string;
 
   @IsInt()
-  @MinLength(1)
-  @MaxLength(3)
   readonly age: number;
 
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly gender: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly image: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly major: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly school: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly job_description: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly company_name: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly company_welfare: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly company_location: string;
 }
