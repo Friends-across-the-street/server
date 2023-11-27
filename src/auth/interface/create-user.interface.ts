ecf0fa000be4786b53d81f5c254469c83976310a
KKardd <wlstjd4013@gmail.com>
2023-11-27 23:54:01 +0900

    feat: add interface about create-student-user

--- src/auth/interface/create-user.interface.ts
+++ src/auth/interface/create-user.interface.ts
@@ -12,3 +12,19 @@ export interface CreateIncumbentUserArgs {
   company_welfare: string;
   company_location: string;
 }
+
+export interface CreateStudentUserArgs {
+  email: string;
+  password: string;
+  name: string;
+  age: number;
+  gender: string;
+  image: string;
+  total_grade: number;
+  major: string;
+  school: string;
+  wish_job_description: string;
+  wish_company_name: string;
+  wish_company_welfare: string;
+  wish_company_location: string;
+}
