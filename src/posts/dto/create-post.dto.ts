2b5ca447bd361a02e881e11abd9096ad3fd7ee60
KKardd <wlstjd4013@gmail.com>
2023-12-13 23:37:26 +0900

    fix: correct directory name

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,6 +1,7 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import { CreatePostArgs } from '../interface/create-post.interface';
 
-export class CreatePostDto {
+export class CreatePostDto implements CreatePostArgs {
   @IsString()
   @MinLength(3)
   @MaxLength(50)
@@ -8,6 +9,6 @@ export class CreatePostDto {
   @IsString()
   @MinLength(3)
   content: string;
-  incumbentUserId: number;
-  studentUserId: number;
+  incumbentUserId: number | null;
+  studentUserId: number | null;
 }
