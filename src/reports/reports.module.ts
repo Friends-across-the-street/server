f03a7fa8e0dba0eba9fd59ae8f7ca3a44bbfdd34
KKardd <wlstjd4013@gmail.com>
2023-12-16 00:23:21 +0900

    feat: configurate reports module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { PrismaService } from 'src/prisma.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   exports: [ReportsService],
-  providers: [ReportsService],
+  providers: [ReportsService, PrismaService],
 })
 export class ReportsModule {}
