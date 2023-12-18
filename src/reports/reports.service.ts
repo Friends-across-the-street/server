58e41bcc2fb008d3eb43ac329b0a69390001e68f
KKardd <wlstjd4013@gmail.com>
2023-12-18 14:14:14 +0900

    feat: add report-post service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { PrismaService } from 'src/prisma.service';
+import { ReportPostArgs } from './interface/report.interface';
 
 @Injectable()
 export class ReportsService {
   constructor(private prismaService: PrismaService) {}
+
+  @UseGuards(AuthGuard)
+  async reportPost(args: ReportPostArgs) {
+    return await this.prismaService.reported_post.create({
+      data: args,
+    });
+  }
 }
