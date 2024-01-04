d62037eed9ead6a080e6a3eb2cf0b34c5b2422c4
KKardd <wlstjd4013@gmail.com>
2024-01-04 23:55:01 +0900

    remove: error interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { IncumbentUsersModule } from './incumbent_users/incumbent_users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import authConfig from './global/config/authConfig';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomExceptionFilter } from './global/custom-exception.filter';
+import { TransformInterceptor } from './global/interceptor/transform.interceptor';
 
 @Module({
   imports: [
@@ -21,6 +24,9 @@ import authConfig from './global/config/authConfig';
     PostsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    { provide: APP_FILTER, useClass: CustomExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+  ],
 })
 export class AppModule {}
