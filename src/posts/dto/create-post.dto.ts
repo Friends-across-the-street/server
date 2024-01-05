d2452d4b58ae97a283a901de6c7a4393200bbaa2
KKardd <wlstjd4013@gmail.com>
2024-01-05 15:20:32 +0900

    refactor: add exception in create-post service

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,16 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 import { CreatePostArgs } from '../interface/create-post.interface';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto implements CreatePostArgs {
   @IsString()
-  @MinLength(3)
+  @MinLength(2)
   @MaxLength(50)
+  @ApiProperty({ description: '제목', type: String })
   title: string;
+
   @IsString()
   @MinLength(3)
+  @ApiProperty({ description: '내용', type: String })
   content: string;
-  incumbentUserId: number | null;
-  studentUserId: number | null;
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { PrismaService } from 'src/prisma.service';
 import { CreatePostArgs } from './interface/create-post.interface';
 import { IncumbentUser, StudentUser } from 'src/global/types/user.type';
 import { UserType } from 'src/auth/enum/user-type.enum';
+import { CustomException } from 'src/global/exception/custom.exception';
 
 @Injectable()
 export class PostsService {
@@ -15,9 +16,12 @@ export class PostsService {
       user.type === UserType.INCUMBENT
         ? { incumbentUserId: user.id }
         : { studentUserId: user.id };
-    await this.prismaService.posts.create({
+    const createdPost = await this.prismaService.posts.create({
       data: { ...args, ...userTypeById },
     });
+    if (!createdPost.incumbentUserId && !createdPost.studentUserId) {
+      throw new CustomException('userId가 일치하지 않습니다.', 404);
+    }
     return;
   }
 
