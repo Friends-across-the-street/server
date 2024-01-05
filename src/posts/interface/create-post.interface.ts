f821cecfe9abdefc6faf5d682db80c2d1814d5c3
KKardd <wlstjd4013@gmail.com>
2024-01-05 14:14:56 +0900

    refactor: apply user data in create-post service

--- src/posts/interface/create-post.interface.ts
+++ src/posts/interface/create-post.interface.ts
@@ -1,6 +1,4 @@
 export interface CreatePostArgs {
   readonly title: string;
   readonly content: string;
-  readonly incumbentUserId: number | null;
-  readonly studentUserId: number | null;
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,16 +2,23 @@ import { Injectable, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { PrismaService } from 'src/prisma.service';
 import { CreatePostArgs } from './interface/create-post.interface';
+import { IncumbentUser, StudentUser } from 'src/global/types/user.type';
+import { UserType } from 'src/auth/enum/user-type.enum';
 
 @Injectable()
 export class PostsService {
   constructor(private prismaService: PrismaService) {}
 
   @UseGuards(AuthGuard)
-  async create(args: CreatePostArgs) {
-    return await this.prismaService.posts.create({
-      data: args,
+  async create(args: CreatePostArgs, user: IncumbentUser | StudentUser) {
+    const userTypeById =
+      user.type === UserType.INCUMBENT
+        ? { incumbentUserId: user.id }
+        : { studentUserId: user.id };
+    await this.prismaService.posts.create({
+      data: { ...args, ...userTypeById },
     });
+    return;
   }
 
   async getPage(page: number, limit: number) {
