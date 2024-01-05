b4ffa7042e1519d2913677787713550c09d72873
KKardd <wlstjd4013@gmail.com>
2024-01-05 15:20:04 +0900

    refactor: add exception in auth

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { incumbent_users, student_users } from '@prisma/client';
+import { CustomException } from 'src/global/exception/custom.exception';
 
 interface RequestUser extends Request {
   user: incumbent_users | student_users;
@@ -20,6 +21,9 @@ export class AuthGuard implements CanActivate {
   }
 
   private async validateRequest(request: RequestUser) {
+    if (!request.headers.authorization) {
+      throw new CustomException('헤더의 Auth 토큰이 존재하지 않습니다', 401);
+    }
     const jwtString = request.headers.authorization.split('Bearer ')[1];
     const payload = await this.authService.verify(jwtString);
     const type = payload.company_name ? 'incumbent' : 'student';
