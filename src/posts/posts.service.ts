9d0bb290f7a9dcc1d5a5b27177aa7bf224f25a1b
KKardd <wlstjd4013@gmail.com>
2024-01-08 15:30:29 +0900

    refactor: modify get-post-page service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -6,6 +6,7 @@ import { IncumbentUser, StudentUser } from 'src/global/types/user.type';
 import { UserType } from 'src/auth/enum/user-type.enum';
 import { CustomException } from 'src/global/exception/custom.exception';
 import { UpdatePostArgs } from './interface/update-post.interface';
+import { postInList } from './interface/post-list.interface';
 
 @Injectable()
 export class PostsService {
@@ -27,13 +28,38 @@ export class PostsService {
   }
 
   async getPage(page: number, limit: number) {
-    return await this.prismaService.posts.findMany({
-      skip: (page - 1) * limit,
-      take: limit,
-      orderBy: {
-        id: 'desc',
-      },
-    });
+    const result = [];
+    const postList = (await this.prismaService
+      .$queryRaw`SELECT p.id AS postId, p.title, p.content, p.view, p.hit, p.createdAt AS postCreateDate, p.updatedAt AS postUpdateDate, i.id AS incumbentId, i.name AS incumbentName, i.image AS incumbentImage, s.id AS studentId, s.name AS studentName, s.image AS studentImage
+    FROM posts AS p
+    LEFT JOIN incumbent_users AS i ON p.incumbent_user_id = i.id
+    LEFT JOIN student_users AS s ON p.student_user_id = s.id
+    ORDER BY postCreateDate DESC
+    LIMIT ${limit} OFFSET ${(page - 1) * limit};`) as postInList[];
+    for (const post of postList) {
+      const type = post.incumbentId === null ? 'student' : 'incumbent';
+      const id = type === 'student' ? post.studentId : post.incumbentId;
+      const name = type === 'student' ? post.studentName : post.incumbentName;
+      const image =
+        type === 'student' ? post.studentImage : post.incumbentImage;
+      const refinedPost = {
+        id: post.postId,
+        title: post.title,
+        content: post.content,
+        view: post.view,
+        hit: post.hit,
+        createdAt: post.postCreateDate,
+        updatedAt: post.postUpdateDate,
+        user: {
+          type,
+          id,
+          name,
+          image,
+        },
+      };
+      result.push(refinedPost);
+    }
+    return result;
   }
 
   async getById(postId: number) {
