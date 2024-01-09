8babb1af639edfb12c0b50bb7ca35ad0041b5eb5
KKardd <wlstjd4013@gmail.com>
2024-01-09 14:59:05 +0900

    docs: modify naming in swagger

--- src/incumbent_users/incumbent_users.controller.ts
+++ src/incumbent_users/incumbent_users.controller.ts
@@ -7,7 +7,7 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 export class IncumbentUsersController {
   constructor(private incumbentUsersService: IncumbentUsersService) {}
 
-  @ApiOperation({ summary: 'UserId로 자세한 현직자 정보 찾기' })
+  @ApiOperation({ summary: 'UserId로 현직자 정보 찾기' })
   @ApiResponse({ status: 200, description: '현직자 정보 찾기 성공' })
   @ApiResponse({ status: 404, description: '현직자 정보 찾을 수 없음' })
   @Get('/:id')
--- src/student_users/student_users.controller.ts
+++ src/student_users/student_users.controller.ts
@@ -6,7 +6,7 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 @Controller('student-users')
 export class StudentUsersController {
   constructor(private studentUsersService: StudentUsersService) {}
-  @ApiOperation({ summary: 'UserId로 자세한 학생 정보 찾기' })
+  @ApiOperation({ summary: 'UserId로 학생 정보 찾기' })
   @ApiResponse({ status: 200, description: '학생 정보 찾기 성공' })
   @ApiResponse({ status: 404, description: '학생 정보를 찾을 수 없음.' })
   @Get('/:id')
