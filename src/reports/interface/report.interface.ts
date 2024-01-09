e8d7e6819e4e481ae30356656e3bfb1853354459
KKardd <wlstjd4013@gmail.com>
2024-01-10 03:15:02 +0900

    fix: apply renaming column

--- src/reports/interface/report.interface.ts
+++ src/reports/interface/report.interface.ts
@@ -1,6 +1,6 @@
 export interface ReportPostArgs {
   readonly postId: number;
   readonly reason: string;
-  readonly incumbentUserId: number | null;
-  readonly studentUserId: number | null;
+  readonly incumbent_id: number | null;
+  readonly student_id: number | null;
 }
