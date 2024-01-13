4118a6576706b0cce9f9530f85033ea49f5c3a4b
KKardd <wlstjd4013@gmail.com>
2024-01-13 11:17:13 +0900

    docs: add swagger in findAll category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
+  @ApiOperation({ summary: '카테고리 전체 조회' })
+  @ApiResponse({ status: 200, description: '전체 조회 성공' })
   @Get()
   async findAll() {
     return await this.categoryService.findAll();
