ff2491710040538ed1756d805778fb066158224c
KKardd <wlstjd4013@gmail.com>
2024-01-17 20:10:08 +0900

    feat: add comment module

---
+++ src/comment/comment.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { PrismaService } from 'src/prisma.service';
+
+@Module({
+  imports: [AuthModule],
+  providers: [CommentService, PrismaService],
+  exports: [CommentService],
+})
+export class CommentModule {}
