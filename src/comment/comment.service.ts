239bb0cb4b80d4cc29bff43911ab040ebe353de3
KKardd <wlstjd4013@gmail.com>
2024-01-19 00:28:13 +0900

    feat: add update-comment service

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UserType } from 'src/auth/enum/user-type.enum';
+import { CustomException } from 'src/global/exception/custom.exception';
+import { UserDataInAuthGuard } from 'src/global/types/user.type';
 import { CreateCommentArgs } from 'src/posts/interface/create-comment.interface';
+import { UpdateCommentArgs } from 'src/posts/interface/update-comment.interface';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -8,5 +12,24 @@ export class CommentService {
 
   async create(args: CreateCommentArgs) {
     return await this.prismaService.comments.create({ data: args });
+  }
+
+  async update(args: UpdateCommentArgs) {
+    const comment = await this.prismaService.comments.findFirst({
+      where: { id: args.commentId },
+    });
+    const commentType =
+      comment.incumbentId === null ? UserType.STUDENT : UserType.INCUMBENT;
+    if (
+      args.user.type !== commentType ||
+      (comment.incumbentId !== args.user.id &&
+        comment.studentId !== args.user.id)
+    ) {
+      throw new CustomException('댓글의 소유자가 아닙니다.', 403);
+    }
+    await this.prismaService.comments.update({
+      data: { content: args.content },
+      where: { id: comment.id },
+    });
   }
 }
