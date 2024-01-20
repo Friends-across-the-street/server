06a6683b862e83c5235d3d0da0f2b17025dd3078
KKardd <wlstjd4013@gmail.com>
2024-01-20 15:57:36 +0900

    feat: add incumbent-add-additional router

--- src/incumbents/incumbents.controller.ts
+++ src/incumbents/incumbents.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { IncumbentService } from './incumbents.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AddAddtionalDto } from './dto/add-addtional.dto';
 
 @ApiTags('INCUMBENT-USER')
 @Controller('incumbent-users')
@@ -10,8 +18,31 @@ export class IncumbentController {
   @ApiOperation({ summary: 'UserId로 현직자 정보 찾기' })
   @ApiResponse({ status: 200, description: '현직자 정보 찾기 성공' })
   @ApiResponse({ status: 404, description: '현직자 정보 찾을 수 없음' })
+  @ApiParam({ name: 'id', type: Number, description: '현직자 ID' })
   @Get('/:id')
   async findById(@Param('id') userId: number) {
     return await this.incumbentService.findById(userId);
+  }
+
+  @ApiOperation({ summary: '현직자 추가 정보 등록' })
+  @ApiResponse({ status: 201, description: '추가 정보 등록 성공' })
+  @ApiResponse({
+    status: 401,
+    description: '헤더의 Auth정보가 존재하지 않습니다.',
+  })
+  @ApiResponse({ status: 403, description: '토큰이 유효하지 않습니다.' })
+  @ApiResponse({
+    status: 404,
+    description: 'userId가 일치하지 않습니다.',
+  })
+  @ApiParam({ name: 'id', type: Number, description: '현직자 ID' })
+  @ApiBearerAuth('access-token')
+  @Post('/additional/:id')
+  @UseGuards(AuthGuard)
+  async addAdditional(
+    @Param('id') userId: number,
+    @Body() dto: AddAddtionalDto,
+  ) {
+    return await this.incumbentService.addAdditional(userId, dto);
   }
 }
