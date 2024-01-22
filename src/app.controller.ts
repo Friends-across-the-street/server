0c9d73aab90452a4623e60b8790bc6e79352a5d0
KKardd <wlstjd4013@gmail.com>
2024-01-22 10:17:04 +0900

    docs: add swagger in healthcheck method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('')
 export class AppController {
+  @ApiOperation({ summary: 'Health Check' })
+  @ApiResponse({ status: 200, description: '서버 생존' })
+  @ApiTags('Health Check')
   @Get('/healthcheck')
   healthcheck() {
     return 'OK';
