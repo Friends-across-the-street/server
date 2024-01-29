ca0aa1cf6b3624ba8369a9199e48071c7b6c1664
KKardd <wlstjd4013@gmail.com>
2024-01-29 10:30:29 +0900

    feat: DI events-module in chat

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,9 +4,10 @@ import { ChatController } from './chat.controller';
 import { MongoPrismaService } from 'src/mongo-prisma.service';
 import { PrismaService } from 'src/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, EventsModule],
   controllers: [ChatController],
   providers: [ChatService, MongoPrismaService, PrismaService],
 })
