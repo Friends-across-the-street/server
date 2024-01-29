e074856275a7ea57c76cfc311c813a65c3c6a784
KKardd <wlstjd4013@gmail.com>
2024-01-29 16:46:58 +0900

    Revert "feat: apply socket in send-chat service"
    This reverts commit ed96ff89519f07f8bfa08385a18585f01823026b.

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,34 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { SendChatDto } from './dto/send.dto';
 import { MongoPrismaService } from 'src/mongo-prisma.service';
-import { onlineMap } from 'src/events/onlingMap';
-import { EventsGateway } from 'src/events/events.gateway';
-import { CustomException } from 'src/global/exception/custom.exception';
-
-function getKeyByValue(object, value) {
-  return Object.keys(object).find((key) => object[key] === value);
-}
 
 @Injectable()
 export class ChatService {
-  constructor(
-    private mongoPrsimaService: MongoPrismaService,
-    private readonly eventsGateway: EventsGateway,
-  ) {}
+  constructor(private mongoPrsimaService: MongoPrismaService) {}
 
   async send(id: number, dto: SendChatDto) {
-    let createdChat;
-    try {
-      createdChat = await this.mongoPrsimaService.chat.create({
-        data: { ...dto },
-      });
-    } catch {
-      throw new CustomException('채팅 메세지 전송에 실패했습니다.', 400);
-    }
-    const receiverSocketId = getKeyByValue(onlineMap[`/`], Number(id));
-    this.eventsGateway.server
-      .to(receiverSocketId)
-      .emit('chatToServer', dto.message);
-    return createdChat;
+    return await this.mongoPrsimaService.chat.create({ data: { ...dto } });
   }
 }
