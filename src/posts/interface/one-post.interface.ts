7aa7a336a7c4948652fe141fd753ad427f76022e
KKardd <wlstjd4013@gmail.com>
2024-01-30 19:11:08 +0900

    refactor: declare get-one-post service types

--- src/posts/interface/one-post.interface.ts
+++ src/posts/interface/one-post.interface.ts
@@ -1,22 +1,66 @@
-export interface onePostIncludeComments {
-  // 게시글 관련
-  readonly postId: number;
-  readonly postIncumbentId: number;
-  readonly postStudentId: number;
-  readonly postTitle: string;
-  readonly postContent: string;
-  readonly postView: number;
-  readonly postRecommend: number;
-  readonly postReported: number;
-  readonly postCreatedDate: Date;
-  readonly postUpdatedDate: Date;
-  // 댓글 관련
-  readonly commentId: number;
-  readonly commentIncumbentId: number;
-  readonly commentStudentId: number;
-  readonly commentContent: string;
-  readonly parentCommentId: number;
-  readonly commentRecommend: number;
-  readonly commentCreatedDate: Date;
-  readonly commentUpdatedDate: Date;
+/** 게시글 쿼리문 반환 타입 */
+export interface onePostForQuery {
+  readonly id: number;
+  readonly incumbentId?: number;
+  readonly studentId?: number;
+  readonly name: string;
+  readonly image?: string;
+  readonly title: string;
+  readonly content: string;
+  readonly view: number;
+  readonly recommend: number;
+  readonly reported: number;
+  readonly createdDate: Date;
+  readonly updatedDate: Date;
+}
+
+/** 게시글 리턴값 */
+export interface refinedOnePost {
+  readonly id: number;
+  readonly user: {
+    readonly incumbentId?: number;
+    readonly studentId?: number;
+    readonly name: string;
+    readonly image?: string;
+  };
+  readonly title: string;
+  readonly content: string;
+  readonly view: number;
+  readonly recommend: number;
+  readonly reported: number;
+  readonly createdDate: Date;
+  readonly updatedDate: Date;
+  readonly isMine: boolean;
+  readonly isRecommend: boolean;
+}
+
+/** 댓글 쿼리문 반환 타입 */
+export interface commentsInPostForQuery {
+  readonly id: number;
+  readonly incumbentId?: number;
+  readonly studentId?: number;
+  readonly name: string;
+  readonly image?: string;
+  readonly content: string;
+  readonly parentCommentId?: number;
+  readonly recommend: number;
+  readonly createdDate: Date;
+  readonly updatedDate: Date;
+}
+
+/** 댓글 리턴값 */
+export interface refinedCommentsInPost {
+  readonly id: number;
+  readonly user: {
+    readonly incumbentId?: number;
+    readonly studentId?: number;
+    readonly name: string;
+    readonly image?: string;
+  };
+  readonly content: string;
+  readonly parentCommentId?: number;
+  readonly recommend: number;
+  readonly createdDate: Date;
+  readonly updatedDate: Date;
+  readonly isMine: boolean;
 }
