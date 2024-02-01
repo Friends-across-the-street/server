5a5bc23ff20f3012bf452102ce5edb8f6a379c7e
KKardd <wlstjd4013@gmail.com>
2024-02-01 11:52:05 +0900

    refactor: add repository layer in user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from 'src/prisma.service';
 import { AuthService } from 'src/auth/auth.service';
+import { UsersRepository } from './users.repository';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, AuthService],
+  providers: [UsersService, PrismaService, AuthService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
