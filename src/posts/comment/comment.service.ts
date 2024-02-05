d57bf368e975d50e865e4a72e4b11fb403bee10f
KKardd <wlstjd4013@gmail.com>
2024-02-05 18:59:17 +0900

    refactor: modify architecture for comment

--- src/posts/comment/comment.service.ts
+++ src/posts/comment/comment.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { UserType } from 'src/auth/enum/user-type.enum';
 import { CustomException } from 'src/global/exception/custom.exception';
 import { UserDataInAuthGuard } from 'src/global/types/user.type';
 import { CreateCommentArgs } from 'src/posts/interface/create-comment.interface';
@@ -18,13 +17,7 @@ export class CommentService {
     const comment = await this.prismaService.comments.findFirst({
       where: { id: args.commentId },
     });
-    const commentType =
-      comment.incumbentId === null ? UserType.STUDENT : UserType.INCUMBENT;
-    if (
-      args.user.type !== commentType ||
-      (comment.incumbentId !== args.user.id &&
-        comment.studentId !== args.user.id)
-    ) {
+    if (comment.userId !== args.user.id) {
       throw new CustomException('댓글의 소유자가 아닙니다.', 403);
     }
     await this.prismaService.comments.update({
@@ -37,14 +30,10 @@ export class CommentService {
     const comment = await this.prismaService.comments.findFirst({
       where: { id: commentId },
     });
-    const commentType =
-      comment.incumbentId === null ? UserType.STUDENT : UserType.INCUMBENT;
-    if (
-      user.type !== commentType ||
-      (comment.incumbentId !== user.id && comment.studentId !== user.id)
-    ) {
+    if (comment.userId !== user.id) {
       throw new CustomException('댓글의 소유자가 아닙니다.', 403);
     }
+
     await this.prismaService.comments.delete({
       where: { id: comment.id },
     });
