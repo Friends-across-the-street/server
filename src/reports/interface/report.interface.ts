95df6aa16f14d23a56ca1f1d455e31addbadcb45
KKardd <wlstjd4013@gmail.com>
2024-02-07 13:41:42 +0900

    refactor: modify interface about report service

--- src/reports/interface/report.interface.ts
+++ src/reports/interface/report.interface.ts
@@ -1,7 +1,9 @@
 import { UserDataInAuthGuard } from 'src/global/types/user.type';
+import { ReportedType } from '../enum/report-type.enum';
 
-export interface ReportPostArgs {
-  readonly postId: number;
+export interface ReportTypeArgs {
+  readonly id: number;
   readonly reason: string;
   readonly user: UserDataInAuthGuard;
+  readonly type: ReportedType;
 }
