85e1e6ea15dd12e37037a9af798d655f8ff055f1
KKardd <wlstjd4013@gmail.com>
2024-02-09 13:33:49 +0900

    refactor: apply reports-repository about report-post service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,10 +3,14 @@ import { AuthGuard } from 'src/auth/auth.guard';
 import { PrismaService } from 'src/prisma.service';
 import { ReportPostArgs } from './interface/report.interface';
 import { CustomException } from 'src/global/exception/custom.exception';
+import { ReportsRepository } from './reports.repository';
 
 @Injectable()
 export class ReportsService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly reportsRepository: ReportsRepository,
+  ) {}
 
   @UseGuards(AuthGuard)
   async reportPost(args: ReportPostArgs) {
@@ -16,17 +20,14 @@ export class ReportsService {
     if (!post) {
       throw new CustomException('게시글이 존재하지 않습니다.', 404);
     }
-    const createdReport = await this.prismaService.reportedPosts.create({
-      data: { postId: args.postId, userId: args.user.id, reason: args.reason },
-    });
-    if (!createdReport.userId) {
-      throw new CustomException('유저가 존재하지 않습니다.', 404);
-    }
+    await this.reportsRepository.reportPost(
+      args.postId,
+      args.reason,
+      args.user,
+    );
     await this.prismaService.posts.update({
       where: { id: args.postId },
       data: { reported: Number(post.reported) + 1 },
     });
   }
-
-  // TODD 댓글 신고 API
 }
