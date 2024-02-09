25ebdfd52dfcb8d99cdf5e5033dbc8278c7583ad
KKardd <wlstjd4013@gmail.com>
2024-02-09 13:33:22 +0900

    feat: add report-post method

--- src/reports/reports.repository.ts
+++ src/reports/reports.repository.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserDataInAuthGuard } from 'src/global/types/user.type';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ReportsRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async reportPost() {}
+  async reportPost(postId: number, reason: string, user: UserDataInAuthGuard) {
+    await this.prismaService.reportedPosts.create({
+      data: {
+        postId,
+        userId: user.id,
+        reason,
+      },
+    });
+  }
 
   async reportComment() {}
 
