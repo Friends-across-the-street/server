a4df5a5615df382003404385fcc4fd4c15356d26
KKardd <wlstjd4013@gmail.com>
2024-02-16 17:55:53 +0900

    feat: add report-user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { userType } from 'prisma/generated/mysql';
 import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RequestUser } from 'src/global/decorator/request-user.decorator';
+import { UserDataInAuthGuard } from 'src/global/types/user.type';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ReportUserDto } from './dto/report.dto';
+import { ReportsService } from 'src/reports/reports.service';
 
 @ApiTags('USER')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly reportsService: ReportsService,
+  ) {}
 
   @ApiOperation({ summary: '유저 정보 조회' })
   @ApiResponse({ status: 200, description: '정보 조회 성공' })
@@ -19,5 +35,20 @@ export class UsersController {
   @Get('/:id')
   async findById(@Param('id') userId: number, @Query('type') type: userType) {
     return await this.usersService.findById(userId, type);
+  }
+
+  @ApiTags('POST')
+  @Post('/report/:userId')
+  @UseGuards(AuthGuard)
+  async report(
+    @Param('userId') userId: number,
+    @Body() dto: ReportUserDto,
+    @RequestUser() user: UserDataInAuthGuard,
+  ) {
+    return await this.reportsService.reportUser({
+      targetUserId: userId,
+      reason: dto.reason,
+      reportingUser: user,
+    });
   }
 }
