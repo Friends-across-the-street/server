ad2ac41ee2af6eb9c58ed6278b57d8cc238bd6c4
KKardd <wlstjd4013@gmail.com>
2024-03-06 12:53:35 +0900

    refactor: add columns in add-additional-info dto

--- src/users/dto/add-additional-info.dto.ts
+++ src/users/dto/add-additional-info.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import {
   AddAdditionalInfoForIncumbentArgs,
   AddAdditionalInfoForStudentArgs,
 } from '../interface/add-additional-info.interface';
+import { dayAndNight } from 'prisma/generated/mysql';
 
 export class AddAdditionalInfoForIncumbentDto
   implements AddAdditionalInfoForIncumbentArgs
@@ -20,11 +27,20 @@ export class AddAdditionalInfoForIncumbentDto
   @ApiProperty({ description: '학부 전공', type: String })
   major: string;
 
+  @IsEnum(dayAndNight)
+  @ApiProperty({ description: '전공 주야구분', type: String })
+  majorDayAndNight: dayAndNight;
+
   @IsString()
   @MinLength(2)
   @MaxLength(50)
   @ApiProperty({ description: '출신 학교', type: String })
   school: string;
+
+  @IsString()
+  @MinLength(2)
+  @ApiProperty({ description: '학교 분교(캠퍼스)', type: String })
+  schoolBranch: string;
 
   @IsString()
   @MinLength(2)
@@ -47,4 +63,51 @@ export class AddAdditionalInfoForIncumbentDto
 }
 
 export class AddAdditionalInfoForStudentDto
-  implements AddAdditionalInfoForStudentArgs {}
+  implements AddAdditionalInfoForStudentArgs
+{
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  @ApiProperty({ description: '희망 세부 업무', type: String })
+  wishJobDescription: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
+  @ApiProperty({ description: '학부 전공', type: String })
+  major: string;
+
+  @IsEnum(dayAndNight)
+  @ApiProperty({ description: '전공 주야구분', type: String })
+  majorDayAndNight: dayAndNight;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  @ApiProperty({ description: '재학 학교', type: String })
+  school: string;
+
+  @IsString()
+  @MinLength(2)
+  @ApiProperty({ description: '학교 분교(캠퍼스)', type: String })
+  schoolBranch: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  @ApiProperty({ description: '희망 회사 이름', type: String })
+  wishCompanyName: string;
+
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(50)
+  @ApiProperty({ description: '희망 회사 복지', type: String })
+  wishCompanyWelfare?: string;
+
+  // TODO 위치 세분화 필요
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  @ApiProperty({ description: '회사 위치', type: String })
+  wishCompanyLocation: string;
+}
