193390f7933880bb0f03dd0a2c99d6bdfd085d37
KKardd <wlstjd4013@gmail.com>
2024-03-09 21:54:21 +0900

    refactor: modify declaring about add-additional-info

--- src/users/dto/add-additional-info.dto.ts
+++ src/users/dto/add-additional-info.dto.ts
@@ -1,113 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEnum,
-  IsOptional,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
-import {
-  AddAdditionalInfoForIncumbentArgs,
-  AddAdditionalInfoForStudentArgs,
-} from '../interface/add-additional-info.interface';
-import { dayAndNight } from 'prisma/generated/mysql';
+import { IsNumber } from 'class-validator';
+import { AddAdditionalInfoArgs } from '../interface/add-additional-info.interface';
 
-export class AddAdditionalInfoForIncumbentDto
-  implements AddAdditionalInfoForIncumbentArgs
-{
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '세부 업무', type: String })
-  jobDescription: string;
+export class AddAdditionalInfo implements AddAdditionalInfoArgs {
+  @IsNumber()
+  @ApiProperty({ description: '학교 ID', type: Number })
+  schoolId: number;
 
-  @IsString()
-  @MinLength(2)
-  @MaxLength(10)
-  @ApiProperty({ description: '학부 전공', type: String })
-  major: string;
+  @IsNumber()
+  @ApiProperty({ description: '전공 ID', type: Number })
+  majorId: number;
 
-  @IsEnum(dayAndNight)
-  @ApiProperty({ description: '전공 주야구분', type: String })
-  majorDayAndNight: dayAndNight;
+  @IsNumber()
+  @ApiProperty({ description: '회사 ID(희망)', type: Number })
+  companyId: number;
 
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '출신 학교', type: String })
-  school: string;
+  @IsNumber()
+  @ApiProperty({ description: '직업 대분류 ID', type: Number })
+  bigJobKindId: number;
 
-  @IsString()
-  @MinLength(2)
-  @ApiProperty({ description: '학교 분교(캠퍼스)', type: String })
-  schoolBranch: string;
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '회사 이름', type: String })
-  companyName: string;
-
-  @IsOptional()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '회사 복지', type: String })
-  companyWelfare?: string;
-
-  // TODO 위치 세분화 필요
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '회사 위치', type: String })
-  companyLocation: string;
-}
-
-export class AddAdditionalInfoForStudentDto
-  implements AddAdditionalInfoForStudentArgs
-{
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '희망 세부 업무', type: String })
-  wishJobDescription: string;
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(10)
-  @ApiProperty({ description: '학부 전공', type: String })
-  major: string;
-
-  @IsEnum(dayAndNight)
-  @ApiProperty({ description: '전공 주야구분', type: String })
-  majorDayAndNight: dayAndNight;
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '재학 학교', type: String })
-  school: string;
-
-  @IsString()
-  @MinLength(2)
-  @ApiProperty({ description: '학교 분교(캠퍼스)', type: String })
-  schoolBranch: string;
-
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '희망 회사 이름', type: String })
-  wishCompanyName: string;
-
-  @IsOptional()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '희망 회사 복지', type: String })
-  wishCompanyWelfare?: string;
-
-  // TODO 위치 세분화 필요
-  @IsString()
-  @MinLength(2)
-  @MaxLength(50)
-  @ApiProperty({ description: '회사 위치', type: String })
-  wishCompanyLocation: string;
+  @IsNumber()
+  @ApiProperty({ description: '직업 중분류 ID', type: Number })
+  midJobKindId: number;
 }
--- src/users/interface/add-additional-info.interface.ts
+++ src/users/interface/add-additional-info.interface.ts
@@ -1,23 +1,7 @@
-import { dayAndNight } from 'prisma/generated/mysql';
-
-export interface AddAdditionalInfoForIncumbentArgs {
-  jobDescription: string;
-  major: string;
-  majorDayAndNight: dayAndNight;
-  school: string;
-  schoolBranch: string;
-  companyName: string;
-  companyWelfare?: string;
-  companyLocation: string;
-}
-
-export interface AddAdditionalInfoForStudentArgs {
-  major: string;
-  majorDayAndNight: dayAndNight;
-  school: string;
-  schoolBranch: string;
-  wishJobDescription: string;
-  wishCompanyName: string;
-  wishCompanyWelfare?: string;
-  wishCompanyLocation: string;
+export interface AddAdditionalInfoArgs {
+  schoolId: number;
+  majorId: number;
+  companyId: number;
+  bigJobKindId: number;
+  midJobKindId: number;
 }
