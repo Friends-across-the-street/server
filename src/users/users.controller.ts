44263e7725df765b884fdb09feaa558f27636229
KKardd <wlstjd4013@gmail.com>
2024-03-20 09:38:21 +0900

    refactor: remove query parameter about find-by-id router

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -75,14 +75,9 @@ export class UsersController {
   @ApiOperation({ summary: '유저 정보 조회' })
   @ApiResponse({ status: 200, description: '정보 조회 성공' })
   @ApiResponse({ status: 404, description: '유저 정보 없음' })
-  @ApiQuery({
-    name: 'type',
-    type: Number,
-    description: '유저 타입(incumbent/student)',
-  })
   @Get('/:id')
-  async findById(@Param('id') userId: number, @Query('type') type: userType) {
-    return await this.usersService.findById(userId, type);
+  async findById(@Param('id') userId: number) {
+    return await this.usersService.findById(userId);
   }
 
   @ApiOperation({ summary: '유저 신고' })
