d3347d8b0ef91837f0140b36b8b06027509c9459
KKardd <wlstjd4013@gmail.com>
2024-03-25 10:40:49 +0900

    docs: add swagger about find-version

--- src/versions/versions.controller.ts
+++ src/versions/versions.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('VERSION')
 @Controller('versions')
 export class VersionsController {
   constructor() {}
 
+  @ApiOperation({ summary: '버전 찾기' })
+  @ApiResponse({ status: 200, description: '버전 찾기 성공' })
   @Get()
   async findVersion() {
     return {
