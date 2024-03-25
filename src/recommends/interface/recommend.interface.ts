790ca9526a9961362644bbcfe1c42378b461bb6f
KKardd <wlstjd4013@gmail.com>
2024-03-25 21:05:29 +0900

    feat: add recommend-comments arg interface

--- src/recommends/interface/recommend.interface.ts
+++ src/recommends/interface/recommend.interface.ts
@@ -4,3 +4,8 @@ export interface RecommendPostArgs {
   readonly postId: number;
   readonly user: UserDataInAuthGuard;
 }
+
+export interface RecommendCommentArgs {
+  readonly commentId: number;
+  readonly user: UserDataInAuthGuard;
+}
