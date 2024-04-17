84f25acafc5dc3c266b14e3274c60ed6bfe65434
KKardd <wlstjd4013@gmail.com>
2024-04-17 18:22:59 +0900

    refactor: remove user-id property in short-spec services

--- src/users/interface/register-short-spec.interface.ts
+++ src/users/interface/register-short-spec.interface.ts
@@ -1,7 +1,6 @@
 import { UserDataInAuthGuard } from 'src/global/types/user.type';
 
 export interface RegisterShortSpecArgs {
-  userId: number;
   spec: string;
   user: UserDataInAuthGuard;
 }
--- src/users/interface/remove-short-spec.interface.ts
+++ src/users/interface/remove-short-spec.interface.ts
@@ -1,6 +1,5 @@
 import { UserDataInAuthGuard } from 'src/global/types/user.type';
 
 export interface RemoveShortSpecArgs {
-  userId: number;
   user: UserDataInAuthGuard;
 }
