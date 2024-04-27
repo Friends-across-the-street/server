c185ca7ee4b0fe602ee870d8febbc891a9d32b21
KKardd <wlstjd4013@gmail.com>
2024-04-27 20:32:55 +0900

    refacor: combine insert-company & insert-company-job script

--- data/insert-company.js
+++ data/insert-company.js
@@ -8,7 +8,13 @@ const URL = config();
 const connection = mysql.createConnection(URL);
 
 // CSV 파일 경로
-const csvFilePath = process.cwd() + '/data/FINAL_COMPANY_INFO.csv';
+const csvFilePath = process.cwd() + '/data/FINAL_COMPANY_INFO_filter.csv';
+
+const bigJobKind = [];
+const midJobKind = [];
+const smallJobKind = [];
+let bigJobIdx = 0;
+let midJobIdx = 0;
 
 // CSV 파일을 MySQL 테이블에 삽입하는 함수
 function insertCsvDataToMysql() {
@@ -18,10 +24,68 @@ function insertCsvDataToMysql() {
   // 스트림에서 데이터를 읽을 때마다 실행되는 콜백 함수
   stream.on('data', async (data) => {
     // MySQL 테이블에 데이터를 삽입하는 쿼리 작성
-    console.log(data);
+
+    // 직업 대분류 넣기
+    if (!bigJobKind.includes(data.big_company_kind)) {
+      bigJobKind.push(data.big_company_kind);
+      bigJobIdx++;
+
+      const insertBigJobQuery = 'INSERT INTO big_job_kind (name) VALUES (?)';
+      await connection.query(
+        insertBigJobQuery,
+        [data.big_company_kind],
+        (error, results, fields) => {
+          if (error) {
+            console.error('An error occurred while inserting data:', error);
+          }
+        },
+      );
+    }
+
+    // 직업 중분류 넣기
+    if (!midJobKind.includes(data.mid_company_kind)) {
+      midJobKind.push(data.mid_company_kind);
+      midJobIdx++;
+
+      const insertMidJobQuery =
+        'INSERT INTO mid_job_kind (bigJobKindId, name) VALUES (?, ?)';
+      await connection.query(
+        insertMidJobQuery,
+        [bigJobIdx, data.mid_company_kind],
+        (error, results, fields) => {
+          if (error) {
+            console.error('An error occurred while inserting data:', error);
+          }
+        },
+      );
+    }
+
+    if (!smallJobKind.includes(data.company_kind)) {
+      smallJobKind.push(data.company_kind);
+      const insertSmallJobQuery =
+        'INSERT INTO small_job_kind (midJobKindId, name) VALUES (?, ?)';
+      await connection.query(
+        insertSmallJobQuery,
+        [midJobIdx, data.company_kind],
+        (error, results, fields) => {
+          if (error) {
+            console.error('An error occurred while inserting data:', error);
+          }
+        },
+      );
+    }
+
+    const bigJobKindId = bigJobKind.indexOf(data.big_company_kind) + 1;
+    const midJobKindId = midJobKind.indexOf(data.mid_company_kind) + 1;
+    const smallJobKindId = smallJobKind.indexOf(data.company_kind) + 1;
+    if (bigJobKindId === 0 || midJobKindId === 0 || smallJobKindId === 0) {
+      throw new Error('이상함');
+    }
+
     const query =
-      'INSERT INTO company (name, address, longitude, latitude, scale, c_group) VALUES (?, ?, ?, ?, ?, ?)';
+      'INSERT INTO company (name, address, longitude, latitude, scale, big_job_kind_id, mid_job_kind_id, small_job_kind_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
     // 쿼리 실행
+
     await connection.query(
       query,
       [
@@ -30,7 +94,9 @@ function insertCsvDataToMysql() {
         data.longitude,
         data.latitude,
         data.company_scale,
-        data.group,
+        bigJobKindId,
+        midJobKindId,
+        smallJobKindId,
       ],
       (error, results, fields) => {
         if (error) {
