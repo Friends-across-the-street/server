190eaee1fcd6babc7854c60949932ffc25c4a91e
KKardd <wlstjd4013@gmail.com>
2024-05-01 17:49:48 +0900

    feat: declare apply-additional-info interface for ai server

---
+++ src/users/interface/apply-additional-info.interface.ts
@@ -0,0 +1,23 @@
+export interface ApplyAdditionalInfoArgs {
+  index: number;
+  sex: number;
+  name: string;
+  company_name: string;
+  company_address: string;
+  longitude: number;
+  latitude: number;
+  company_scale: string;
+  company_kind: string;
+  big_company_kind: string;
+  mid_company_kind: string;
+  group: number;
+  job: string;
+  univ_kind: string;
+  univ_loc: string;
+  univ_name: string;
+  univ_main_branch: string;
+  major: string;
+  univ_day_night: string;
+  field: string;
+  education: string;
+}
